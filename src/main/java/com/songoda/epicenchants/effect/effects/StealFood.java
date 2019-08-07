b6f83f95284701a028eec532f9034e7f5b00cc43
Brianna <bri@songoda.com>
2019-08-06 21:11:25 -0400

    steal food.

--- src/main/java/com/songoda/epicenchants/effect/effects/StealFood.java
+++ src/main/java/com/songoda/epicenchants/effect/effects/StealFood.java
@@ -2,6 +2,7 @@ package com.songoda.epicenchants.effect.effects;
 
 import com.songoda.epicenchants.effect.EffectExecutor;
 import com.songoda.epicenchants.enums.EventType;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
